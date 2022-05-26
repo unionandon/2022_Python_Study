69084078de5fb408d31e02a51a0da0226844e7d9
unionandon <94822285+unionandon@users.noreply.github.com>
2022-05-26 16:36:46 +0900

    Study_3주차_148~180
    read excel and after filtering, write new excel file

---
+++ 3주차/3주차_Excel_filtering.py
@@ -0,0 +1,9 @@
+## 2022-05-26 
+'''
+ "Variants_2022_05.xlsx" "Sheet2" 를 읽어와  
+ frameshift variant를 가진 행 중에 Exon Number가 5 이상인 행을 가져온다.
+
+Gene Name	Exon Number  HGVS pDot	Sequence Ontology Chr:Pos
+
+헤더는 위의 순서대로 가져와서 새로운 엑셀 파일을 생성한다.
+'''
